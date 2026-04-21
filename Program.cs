57dca9b4a96eea2fca39f2faac9249d9b6e59f74
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-21 05:25:55 +0300

    Differnetiate Write and WriteLine

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿Console.WriteLine("Hell World!");
+﻿// Console.WriteLine("Hell World!");
 // Error in C# console.WriteLine("Hell World!");, Console.WriteLine('Hell World!');
+
+// Console.Write("Congratulations");
+// Console.Write(" ");
+// Console.Write(("You wrote your first lines of code in Csharp"));
+
+Console.WriteLine("Congratulations!");
+Console.WriteLine("You wrote your first lines of code");
