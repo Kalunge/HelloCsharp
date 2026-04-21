bf7bcdd5d2442a77cfed0e9d302f08a003c121f2
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-21 06:05:54 +0300

    Add Variable declarations

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿
-// Boolean Literals
-Console.WriteLine(true);
-Console.WriteLine(false);
+﻿// Variables
+string firstName;
+string FirstName;
+// The abvove two are different
+
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
