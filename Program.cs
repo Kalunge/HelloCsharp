1c466530218cfebf75f1d65a602d4126c3828d2c
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-21 05:50:29 +0300

    Add, string, char and integer literals

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
-﻿// Console.WriteLine("Hell World!");
-// Error in C# console.WriteLine("Hell World!");, Console.WriteLine('Hell World!');
+﻿// String Literals
+Console.WriteLine("Hello World!");
 
-// Console.Write("Congratulations");
-// Console.Write(" ");
-// Console.Write(("You wrote your first lines of code in Csharp"));
 
-Console.WriteLine("Congratulations!"); 
-// Console -> Class, WriteLine -> Method, "Congratulations!" -> Literal String, . -> Member access operator
-Console.WriteLine("You wrote your first lines of code");
+// Caharacter Literals
+Console.WriteLine('b');
+// Console.WriteLine('Hello World');
 
-// Challenge1
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the Second Line.");
+// integer Literals
+Console.WriteLine(123);
