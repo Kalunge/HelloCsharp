b67e5142047e2ef1159c8b77340e830d3e4b1929
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-21 05:59:06 +0300

    Add boolean literals

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿
-// floatpoint literals
-Console.WriteLine(0.25f);
-
-
-Console.WriteLine(2.625);
-
-Console.WriteLine(12.39816m);
+// Boolean Literals
+Console.WriteLine(true);
+Console.WriteLine(false);
