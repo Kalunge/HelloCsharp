dace2d6c8aa1aca3dce21040474c067f980126b2
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-22 05:49:43 +0300

    String interpolation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
-﻿string firstName = "Bob";
-string greeting = "Hello";
-Console.WriteLine(greeting + " " + firstName + "!");
+﻿string projectName = "First-Project";
+Console.WriteLine($@"C:\output\{projectName}\Data");
