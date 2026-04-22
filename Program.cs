c9b878edb7ad727ab2e6d39def0cf004ec7169b4
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-22 05:41:03 +0300

    String Concatenation

--- Program.cs
+++ Program.cs
@@ -1,20 +1,3 @@
-﻿// Invoice generating App
-
-Console.WriteLine("Generating Invoices for customer \"Contoso Corp\" ... \n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoide: 1022\t\tComplete!");
-Console.Write("\nOutput Directory:\t");
-
-Console.Write(@"c:\invoices");  
-
-// Verbatim String literal
-// Console.WriteLine(@" c:\source\repos
-//     (this is where your code goes)");
-// Kon'nichiwa World
-// Console.WriteLine("\u3053\u3093\u306B\u306F World!");
-    
-// Generating Japanese Invoices
-// Nihon no seikyu-sho o seisei suru ni wa:
-Console.Write("\n\nu65e5\u672c\u306e\ubacb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t"); 
-// User command to run the application
-Console.WriteLine(@"c:\invoices\AppContext.exe -j");
+﻿string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
