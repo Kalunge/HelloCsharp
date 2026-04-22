c0851c1e86d6247f1116c2303ea526bc1e946af4
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-22 06:27:18 +0300

    Peform addition/concatenation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-﻿string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u04441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-string englishOutput = "View English output:";
-
-Console.WriteLine($@"{englishOutput} 
-    c:Exercise\{projectName}\data.txt");
-
-Console.WriteLine($@"{russianMessage}:
-    c:\Exercise\{projectName}\ru-Ru\data.txt");
-
+﻿string firstName = "Bob";
+int widgetSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetSold + 7) + " widgets.");
