cf9b7f68e7698e01bb8cf806e52a90035d5e146f
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-22 06:35:46 +0300

    Type casting

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-﻿int sum = 7 + 5;
-int difference = 7 - 5;
-int product = 7 * 5;
-int quotient = 7 / 5;
+﻿int first = 7;
+int second = 5;
 
-
-Console.WriteLine("sum " + sum);
-Console.WriteLine("Difference " + difference);
-Console.WriteLine("Product " + product);
-Console.WriteLine("Quotient " + quotient);
+decimal quotient = (decimal)first / (decimal) second;
+Console.WriteLine(quotient);
