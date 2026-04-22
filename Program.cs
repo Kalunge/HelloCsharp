9e75cb715a1d9e15f8841634efc479315b01c969
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-22 06:53:46 +0300

    incrememnt and orde rof operations

--- Program.cs
+++ Program.cs
@@ -1,19 +1,7 @@
 ﻿int value = 1;
-
-value = value + 1;
-Console.WriteLine("First Incrememnt: " + value);
-
-value += 1;
-Console.WriteLine("Second Incrememnt: " + value);
-
 value++;
-Console.WriteLine("Third incremmnt: " + value);
 
-value = value - 1;
-Console.WriteLine("First decrememnt: " + value);
-
-value -= 1;
-Console.WriteLine("Second decemrent: " + value);
-
-value--;
-Console.WriteLine("Third decrement: " + value);
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
