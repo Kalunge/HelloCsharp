9fd7eef1df1317b66af7319fde51df608a558427
Titus Muthomi <muthomititus+github@gmail.com>
2026-04-27 05:46:34 +0300

    Formatting challenge

--- Challenge1.cs
+++ Challenge1.cs
@@ -2,7 +2,9 @@ public class Test
 {
     public static void DoWork()
     {
-        codeComments();
+        // codeComments();
+        // checkOrders();
+        formatCodeChallenge();
     }
 
 
@@ -24,4 +26,50 @@ public class Test
         // Console.WriteLine($"{firstName} sold {widgetsSold} widgets");
         Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets");
     }
+
+    private static void checkOrders()
+    {
+        Random random = new Random();
+        string[] orderIds = new string[5];
+        
+        for (int i = 0; i < orderIds.Length; i++)
+        {
+            int prefixValue = random.Next(65, 70);
+
+            string prefix = Convert.ToChar(prefixValue).ToString();
+
+            string suffix = random.Next(1, 1000).ToString("000");
+
+            orderIds[i] = prefix + suffix;
+        }
+
+        foreach (var orderId in orderIds)
+        {
+            Console.WriteLine(orderId);
+        }
+    }
+
+    private static void formatCodeChallenge()
+    {
+        string sentence = "The quick brown fox jumps over the lazy dog.";
+
+        char[] charMessage = sentence.ToCharArray();
+
+        Array.Reverse(charMessage);
+        int charCount = 0;
+
+        foreach (var letter in charMessage)
+        {
+            if (letter == 'o')
+            {
+                charCount++;
+            }
+        }
+
+        string newMessage = new string(charMessage);
+
+        Console.WriteLine(newMessage);
+        Console.WriteLine($"'o' appears {charCount} times.");
+    }
+
 }
